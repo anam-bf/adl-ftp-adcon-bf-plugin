84586b4573217db8190232afdf22cdd884c378cd
Erick Otenyo <otenyo.erick@gmail.com>
2025-02-19 06:36:26 +0000

    Implement filtering files by date

--- plugins/adl_ftp_adcon_bf_plugin/src/adl_ftp_adcon_bf_plugin/decoders/adcon_bf.py
+++ plugins/adl_ftp_adcon_bf_plugin/src/adl_ftp_adcon_bf_plugin/decoders/adcon_bf.py
@@ -1,8 +1,8 @@
 import logging
 
 import pandas as pd
-
 from adl_ftp_plugin.registries import FTPDecoder
+from adl_ftp_plugin.utils import get_dates_to_now
 
 logger = logging.getLogger(__name__)
 
@@ -15,6 +15,23 @@ class AdconBFDecoder(FTPDecoder):
     type = "adcon_bf"
     compat_type = "adcon_bf"
     display_name = "ADCON FTP Burkina Faso"
+    
+    def get_matching_files(self, station_link, files):
+        # get all the initial matching files
+        matching_files = super().get_matching_files(station_link, files)
+        
+        # get the dates we need to check
+        dates = get_dates_to_now(date_granularity=station_link.date_granularity,
+                                 timezone=station_link.timezone,
+                                 from_date=station_link.start_date,
+                                 as_string=True,
+                                 str_format="%Y%m%d")
+        
+        # sample filename 0-854-0-004-20250216.txt
+        # filter the matching files by date
+        matching_files = [file for file in matching_files if any(date in file for date in dates)]
+        
+        return matching_files
     
     def decode(self, file_path):
         df = pd.read_csv(file_path, delimiter="\t", decimal=",", dtype={"Heure": str})
